f248be8e6ad0a561f073738e3aa164fbf833da49
willyishmael <wtangka22@gmail.com>
2023-07-14 17:13:41 +0800

    add music controller

--- playlist-api/index.js
+++ playlist-api/index.js
@@ -9,6 +9,9 @@ let playlist = [
     { title: "IT'S YOU", artist: ['MAX', 'Keshi'], album: "IT'S YOU (feat. Keshi)" },
 ];
 
+let nowPlayingIndex = 0;
+let nowPlaying = null;
+
 app.get('/playlist', (req, res) => {
     res.json(playlist)
 });
@@ -17,9 +20,55 @@ app.post('/playlist', (req, res) => {
     playlist.push(req.body);
     res.json({
         message: "Success",
-        data: req.body
+        song: req.body
     });
 });
+
+app.get('/nowplaying', (req, res) => {
+    if (nowPlaying === null) {
+        res.json({
+            message: "No music is playing"
+        })
+    } else {
+        res.json({
+            message: "Now playing",
+            song: nowPlaying
+        })
+    }
+})
+
+app.get('/play', (req, res) => {
+    if (nowPlaying === null) {
+        nowPlayingIndex = 0;
+        nowPlaying = playlist[nowPlayingIndex]
+    }
+
+    res.json({
+        message: "Play",
+        song: nowPlaying
+    })
+});
+
+app.get('/stop', (req, res) => {
+    nowPlaying = null;
+    res.json({
+        message: "Stop",
+    })
+})
+
+app.get('/next', (req, res) => {
+    if (nowPlayingIndex < playlist.length-1) {
+        nowPlayingIndex++
+    } else {
+        nowPlayingIndex = 0
+    }
+    
+    nowPlaying = playlist[nowPlayingIndex]
+    res.json({
+        message: "Next",
+        song: nowPlaying
+    })
+})
 
 const port = 3000;
 app.listen(port, () => {
